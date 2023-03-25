2856e657c9fa5488ac465b22be118f6d5da466f9
shirrra <shirrra@gmail.com>
2023-03-25 23:41:28 +0300

    - in 'hexagen.py', updated the method 'shape.copy_paste()' to support 'source' and 'destination' parameters - changed names of gold files

--- docs/gen_examples.py
+++ docs/gen_examples.py
@@ -1,14 +1,18 @@
-# import sys
-# sys.path.append('../src')
+'''
+This script was used to create plots for the projects USAGE.md file
+'''
 
 from src.hexagen import HexagonsGame, Tile, Shape, Line, Circle, Triangle
 
 HexagonsGame.start()
 
-HexagonsGame.record_step(step_name='1')
-Tile(column=7, row=5).neighbors().draw(color='yellow')
-
-print(HexagonsGame.get_record(step_names=['1'])._linds)
+center_tile1 = Tile(4, 4)
+circle1 = Circle(center_tile=center_tile1, radius=2)
+circle1.draw('black')
+center_tile2=Tile(14, 6)
+circle2 = circle1.copy_paste(source=center_tile1, destination=center_tile2)
+center_tile1.draw('yellow')
+center_tile2.draw('red')
 
 file_name = input('please enter file name\n')
 if file_name == '':
