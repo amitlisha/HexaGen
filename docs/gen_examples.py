a412c8609f86a844a5d11b2e7def7861d946105f
shirrra <shirrra@gmail.com>
2023-03-26 15:30:09 +0300

    - fixed issues with path to data files (constants.py, reading_tasks.py) - added multi-plotting abilities to HexagonsGame.plot() (hexagen.py, plot_board)

--- docs/gen_examples.py
+++ docs/gen_examples.py
@@ -6,13 +6,6 @@ from src.hexagen import HexagonsGame, Tile, Shape, Line, Circle, Triangle
 
 HexagonsGame.start()
 
-center_tile1 = Tile(4, 4)
-circle1 = Circle(center_tile=center_tile1, radius=2)
-circle1.draw('black')
-center_tile2=Tile(14, 6)
-circle2 = circle1.copy_paste(source=center_tile1, destination=center_tile2)
-center_tile1.draw('yellow')
-center_tile2.draw('red')
 
 file_name = input('please enter file name\n')
 if file_name == '':
